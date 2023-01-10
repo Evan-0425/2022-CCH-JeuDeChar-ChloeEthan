ac26754a5eb419895a7e6e1ab31ef8ce54842fac
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-01-10 15:45:15 +0100

    Supression des commentaires inutiles

--- deplacement_coordonnee.py
+++ deplacement_coordonnee.py
@@ -1,16 +1,6 @@
 #
 # https://www.dexterindustries.com/BrickPi/
 # https://github.com/DexterInd/BrickPi3
-#
-# Copyright (c) 2016 Dexter Industries
-# Released under the MIT license (http://choosealicense.com/licenses/mit/).
-# For more information, see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
-#
-# This code is an example for running all motors while a touch sensor connected to PORT_1 of the BrickPi3 is being pressed.
-#
-# Hardware: Connect EV3 or NXT motor(s) to any of the BrickPi3 motor ports. Make sure that the BrickPi3 is running on a 9v power supply.
-#
-# Results:  When you run this program, the motor(s) speed will ramp up and down while the touch sensor is pressed. The position for each motor will be printed.
 
 from __future__ import print_function  # use python 3 syntax but make it compatible with python 2
 from __future__ import division
